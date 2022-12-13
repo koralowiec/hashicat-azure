2544b3a025cf56b389077dd7f2e3812d519b2eca
koralowiec <arek0906@protonmail.com@>
2022-12-13 11:11:06 +0000

    Replaced source for network module

--- network.tf
+++ network.tf
@@ -1,4 +1,5 @@
 module "network" {
-  source              = "Azure/network/azurerm"
+  source  = "app.terraform.io/tf-cloud-azure-workshop/network/azurerm"
+  version = "3.5.0"
   resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
